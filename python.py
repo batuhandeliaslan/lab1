a4176d29be9540f946ceb30a7cf9857b443a661a
Batuhan Deliaslan <153916419+batuhandeliaslan@users.noreply.github.com>
2025-02-20 15:05:55 +0300

    Create python.py

---
+++ python.py
@@ -0,0 +1,31 @@
+
+
+"""name = input("What is your name")
+print("Your name is ",name)
+print("Hello",name)
+Id= input("What is your Id?")
+print("Your Id is" ,Id)"""
+
+"""number1 = input("What is your value for number1")
+number2 = input("what is your value for number2")
+sum=number1+number2
+diff=number1-number2
+prod=number1+number2
+print(number1,number2,sum,diff,prod)"""
+
+"""name=input("What is your name\n")
+lab=float(input("What is your grade\n"))
+midterm=float(input("What is your midterm grade\n"))
+final=float(input("What is your final grade\n"))
+lastscore=lab*0.25+midterm*0.35+final*0.4
+print("Name:"+name)
+print("Lab:",lab)
+print("Midterm:",midterm)
+print("Final:",final)
+print("Last Score:",lastscore)"""
+#print("*\n""**\n""***\n""**\n""*")
+
+
+
+
+
