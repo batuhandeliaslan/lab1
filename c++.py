f7418b0e8cd7363a4748a3ea710041c65c94ba86
Batuhan Deliaslan <153916419+batuhandeliaslan@users.noreply.github.com>
2025-02-20 15:07:11 +0300

    Create c++.py

---
+++ c++.py
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    /*string name;
+    int sid;
+    cout << "What is your name" << endl;
+    cin>>name;
+    cout<<"Hello"<<name<<endl;
+    cout<<"What is your id"<<endl;
+    cin>>sid;
+    cout<<"your id is"<<" "<<sid<<endl;
+    return 0;*/
+    /*float var1,var2,sum,diff,prod;
+    cout<<"Enter your first number"<<endl;
+    cin>>var1;
+    cout<<"Enter your second number"<<endl;
+    cin>>var2;
+    sum=var1+var2;
+    diff=var1+var2;
+    prod=var1*var2;
+    cout<<var1<<var2<<" "<<sum<<" "<<diff<<" "<<prod<<endl;
+    return 0;*/
+    /*string name;
+    float lab,midterm,finall,lastscore;
+    cout<<"What is your name"<<endl;
+    cin>>name;
+    cout<<"WHAT İS YOUR LAB GRADE?"<<endl;
+    cin>>lab;
+    cout<<"What is your midterm grade?"<<endl;
+    cin>>midterm;
+    cout<<"What is your final grade"<<endl;
+    cin>>finall;
+    lastscore=lab*0.25+midterm*0.35+finall*0.40;
+    cout<<"Name:"<<name<<endl<<"Lab"<<endl<<"Midterm:"<<midterm<<endl<<"Final:"<<finall<<endl<<"Last Socre:"<<lastscore<<endl;
+    return 0;
+
+    cout<<"*\n"<<"**\n"<<"***\n"<<"**\n"<<"*\n"<<endl;
+    return 0;*/
+
+}
